2b99418277c4a6c3b105a9fe101cae47d7e8ef6c
Suhani-1412 <141015571+Suhani-1412@users.noreply.github.com>
2025-09-25 17:04:06 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,13 +1,6 @@
 from booking.booking import Booking
 import time
-# 1 way to instantiate the class
-# bot=Booking()
-# bot.land_first_page()
 
-
-#2 way to instantiate it butwith exit method
-# jese hi indentation s bhr aega exit apne aap chl jaega
-#her ethe bot is treated as context manager in which exit is run automatically after the indentation ends
 with Booking(teardown=True) as bot:
     bot.land_first_page()
     bot.handle_popup()
@@ -17,5 +10,5 @@ with Booking(teardown=True) as bot:
     bot.select_adults(count=10)
     bot.clicksearch()
     bot.apply_filtration()
-    time.sleep(8)   #8 sec rukega close hone s pehle
+    time.sleep(8)   
  
